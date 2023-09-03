74ab86fd542bceb88e0662528ce22cd5301b288b
Vishnu Kumar <jstmevishnu@gmail.com>
2023-09-03 20:27:36 +0530

    added flights create crud

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 
 const CityController=require('../../controllers/city-controller');
+const FlightController = require('../../controllers/flight-controller');
 
 const router = express.Router();
 
@@ -9,6 +10,8 @@ router.delete('/city/:id',CityController.destory);
 router.get('/city/:id',CityController.get);
 router.get('/city',CityController.getAll);
 router.patch('/city/:id',CityController.update);
+
+router.post('/flights', FlightController.create);
 
 
 
